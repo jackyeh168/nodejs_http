ab3754a82dc070d2766eb8e32502077840c04000
Jack Yeh <jack.yeh@mile.cloud>
2020-04-13 17:54:22 +0800

    git init

---
+++ index.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+let morgan = require('morgan')
+
+app.use(morgan('combined'))
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+let port = process.env.PORT || 80;
+
+app.listen(port, function () {
+  console.log('Example app listening on port: ', port);
+});
